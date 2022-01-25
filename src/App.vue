0242e40bac5fdef0b033912ec9df4a1f91680c46
Alaaeddine Meddeb <alaaeddinemeddeb@gmail.com>
2022-01-25 10:52:21 +0100

    add: training starter

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,14 @@ export default {
 </script>
 
 <style>
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,6 +31,20 @@ export default {
 }
 
 body {
+  box-sizing: border-box;
   padding: 20px;
+  background: #edf2f7;
+}
+
+.card {
+  padding: 25px;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  border-radius: 7px;
+  background: #fff;
+}
+
+input {
+  height: 26px;
+  padding: 0 10px;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,71 @@
 <template>
-  <div>
-    <v-banner outlined rounded> </v-banner>
+  <div class="home">
+    <div class="add-form card">
+      <form>
+        <input type="text" placeholder="Name" required />
+        <input type="email" placeholder="Email" required />
+        <input type="button" value="Save" />
+      </form>
+    </div>
+    <div class="people-wrap">
+      <div class="people-card card">
+        <div class="avatar">A</div>
+        <div class="name">Alaa</div>
+        <div class="email">alaa@gmail.com</div>
+        <div class="dep">Engineering</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      departments: ["Customer Support", "Engineering", "Sales"],
+    };
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.add-form {
+  input {
+    margin-right: 15px;
+  }
+}
+.people-wrap {
+  margin-top: 20px;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+}
+
+.people-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .name {
+    font-size: 18px;
+    font-weight: 700;
+  }
+  .email {
+    font-size: 14px;
+    color: grey;
+  }
+  .dep {
+    color: rgb(159, 122, 234);
+  }
+}
+.avatar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  aspect-ratio: 1 / 1;
+  border-radius: 50%;
+  background: lightblue;
+  color: white;
+  font-size: 24px;
+}
+</style>
